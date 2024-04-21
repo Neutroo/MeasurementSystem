4abd667d9059bc9be306dc935d33523ef368a66a
Rodion Kushnarenko <rodionkush903@gmail.com>
2024-04-21 20:45:02 +0300

    Revert "added client"
    This reverts commit eecb8baa9cfcfdee6751c60e26687a061be1b259.

--- measurementsystem.client/src/components/icons/IconLock.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <svg width="24px" height="24px" viewBox="-1.2 -1.2 26.40 26.40" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C9.238 2 7 4.238 7 7v3h-.4c-.88 0-1.6.72-1.6 1.6v7C5 19.92 6.08 21 7.4 21h9.2c1.32 0 2.4-1.08 2.4-2.4v-7c0-.88-.72-1.6-1.6-1.6H17V7c0-2.762-2.238-5-5-5Zm3 8V7c0-1.658-1.342-3-3-3S9 5.342 9 7v3h6Z" fill="#339989"></path></g></svg>
-</template>
--- measurementsystem.client/src/main.js
+++ measurementsystem.client/src/main.js
@@ -1,14 +1,6 @@
-/*import './assets/main.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')*/
-
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
